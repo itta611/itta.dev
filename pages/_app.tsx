7d0f44f6806232dbdcc128c837deaed2f4bab7b0
Itta Funahashi <itta@itta.dev>
2022-12-18 17:48:45 +0900

    remove custom theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,7 @@
 import type { AppProps } from 'next/app';
-import { extendTheme } from '@chakra-ui/react';
-
-const theme = extendTheme({
-  fonts: {
-    heading: `Futura 'Open Sans', sans-serif`,
-  },
-});
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} theme={theme} />;
+  return <Component {...pageProps} />;
 }
 
 export default App;
