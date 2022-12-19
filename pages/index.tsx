a18f9ee05771f4f910afe7bf90cb3814355a69f1
Itta Funahashi <itta@itta.dev>
2022-12-19 16:27:11 +0900

    add logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import { Box, Container, Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Logo from 'components/Logo';
 
 const Home: NextPage = () => {
   return (
-    <Box>
+    <Container maxW="container.lg">
       <Head>
         <title>Itta&apos;s Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container maxW="container.lg">
-        <Heading>Itta&apos;s Portfolio</Heading>
-      </Container>
-    </Box>
+      <Box as={'header'}>
+        <Logo mx="auto" />
+      </Box>
+    </Container>
   );
 };
 
