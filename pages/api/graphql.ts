c6efcb70a65a635bbaf9c5c784f85b34c412951c
Itta Funahashi <itta@itta.dev>
2023-01-02 18:38:24 +0900

    add graphql

---
+++ pages/api/graphql.ts
@@ -0,0 +1,42 @@
+import { ApolloServer, gql } from 'apollo-server-micro';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World',
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
+
+  await startServer;
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
