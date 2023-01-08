62c8e0d2d92aed7755612a73bb6f115660b82149
Itta Funahashi <itta@itta.dev>
2023-01-08 20:00:41 +0900

    set default configString

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -4,10 +4,30 @@ import { Box } from '@chakra-ui/react';
 import GraphQLPlayground from 'components/GraphQLPlayground';
 import { NextPage } from 'next';
 
+const configString = `{
+  "editor.cursorShape": "line",
+  "editor.fontFamily": "'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace",
+  "editor.fontSize": 12,
+  "editor.reuseHeaders": true,
+  "editor.theme": "dark",
+  "general.betaUpdates": false,
+  "prettier.printWidth": 80,
+  "prettier.tabWidth": 2,
+  "prettier.useTabs": false,
+  "request.credentials": "omit",
+  "request.globalHeaders": {},
+  "schema.disableComments": true,
+  "schema.polling.enable": true,
+  "schema.polling.endpointFilter": "*localhost*",
+  "schema.polling.interval": 2000,
+  "tracing.hideTracingResponse": true,
+  "tracing.tracingSupported": true
+}`;
+
 const Home: NextPage = () => {
   return (
     <Box overflow="hidden">
-      <GraphQLPlayground endpoint={process.env.NEXT_PUBLIC_API_URL} />
+      <GraphQLPlayground endpoint={process.env.NEXT_PUBLIC_API_URL} configString={configString} />
     </Box>
   );
 };
