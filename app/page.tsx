7e6dad16fcb5f2eb1b052a1e23e6603c13f9f022
Itta Funahashi <itta@itta.dev>
2025-07-02 20:37:23 +0900

    feat: リンクメニューを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
 import Logo from "@/components/Logo";
 import AboutCard from "./cards/about";
 import { Button } from "@/components/ui/button";
+import {
+  IconBrandDiscord,
+  IconBrandGithub,
+  IconBrandX,
+} from "@tabler/icons-react";
 import { DicesIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="max-w-[800px] mx-auto items-center">
-      <div className="mx-9 py-9 flex">
+    <div className="max-w-[800px] px-9 mx-auto items-center">
+      <div className="py-9 flex">
         <Logo />
       </div>
-      <div className="mx-9 py-8 px-12 bg-gray-800 border rounded-2xl">
+      <div className="py-8 px-12 bg-gray-800 border rounded-2xl">
         <AboutCard />
         <div className="flex justify-end mt-8">
           <Button size="lg">
@@ -17,6 +23,21 @@ export default function Home() {
             再表示
           </Button>
         </div>
+      </div>
+      <div className="absolute bottom-6 [&_button]:size-10 space-x-2">
+        <Link href="https://twitter.com/IttaFunahashi" target="_blank">
+          <Button>
+            <IconBrandX className="size-5" />
+          </Button>
+        </Link>
+        <Link href="https://github.com/itta611" target="_blank">
+          <Button>
+            <IconBrandGithub className="size-5" />
+          </Button>
+        </Link>
+        <Button>
+          <IconBrandDiscord className="size-5" />
+        </Button>
       </div>
     </div>
   );
