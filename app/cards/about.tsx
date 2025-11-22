5f6ebfee2fd11ff03ae0f24e34a98e901998eace
Itta Funahashi <itta@itta.dev>
2025-11-22 18:03:49 +0900

    feat: About card

--- app/cards/about.tsx
+++ app/cards/about.tsx
@@ -1,56 +1,61 @@
-import { FC, Fragment } from "react";
 import { CakeIcon, CodeIcon, HammerIcon, MapPinIcon } from "lucide-react";
 import Image from "next/image";
+import { type FC, Fragment } from "react";
 
 const AboutCard: FC = () => {
-  const AboutItems = [
-    {
-      icon: CakeIcon,
-      text: "2008/6/11 (16さい)",
-    },
-    {
-      icon: MapPinIcon,
-      text: "Aichi, Japan",
-    },
-    {
-      icon: CodeIcon,
-      text: "Web Frontend, UI Design",
-    },
-    {
-      icon: HammerIcon,
-      text: "TypeScript / React / Next.js / Blender",
-    },
-  ];
-  return (
-    <>
-      <div className="font-bold relative text-3xl mb-6 inline-block before:absolute before:-left-2 before:-right-2 before:bottom-0 before:h-4 before:rounded-xs before:bg-gray-700 before:transform">
-        <span className="relative z-10">About</span>
-      </div>
-      <div className="flex justify-between items-start">
-        <div className="flex flex-col">
-          <span className="font-bold mb-6">
-            船橋一汰 / Itta Funahashi{" "}
-            <span className="text-slate-400">(@itta611)</span>
-          </span>
-          <div className="grid grid-cols-[auto_1fr] gap-x-3 gap-y-4 text-sm">
-            {AboutItems.map((item, index) => (
-              <Fragment key={index}>
-                <item.icon size={18} className="text-teal-200" />
-                <span className="font-semibold">{item.text}</span>
-              </Fragment>
-            ))}
-          </div>
-        </div>
-        <Image
-          src="/icon.png"
-          alt="Icon"
-          height={512}
-          width={512}
-          className="w-58 h-58 -m-6.5 -mt-8"
-        />
-      </div>
-    </>
-  );
+	const birthday = new Date(2008, 5, 11);
+	const age = Math.floor(
+		(Date.now() - birthday.getTime()) / (1000 * 60 * 60 * 24 * 365.25),
+	);
+
+	const AboutItems = [
+		{
+			icon: CakeIcon,
+			text: `2008/6/11 (${age} さい)`,
+		},
+		{
+			icon: MapPinIcon,
+			text: "Aichi, Japan",
+		},
+		{
+			icon: CodeIcon,
+			text: "Web Frontend, UI Design",
+		},
+		{
+			icon: HammerIcon,
+			text: "TypeScript / React / Next.js / Blender",
+		},
+	];
+	return (
+		<>
+			<div className="font-bold relative text-3xl mb-6 inline-block before:absolute before:-left-2 before:-right-2 before:bottom-0 before:h-4 before:rounded-xs before:bg-gray-700 before:transform">
+				<span className="relative z-10">About</span>
+			</div>
+			<div className="flex justify-between items-start">
+				<div className="flex flex-col">
+					<span className="font-bold mb-8 text-lg">
+						船橋一汰 / Itta Funahashi{" "}
+						<span className="text-slate-400">(@itta611)</span>
+					</span>
+					<div className="grid grid-cols-[auto_1fr] gap-x-3 gap-y-4 text-sm">
+						{AboutItems.map((item) => (
+							<Fragment key={item.text}>
+								<item.icon size={18} className="text-teal-200" />
+								<span className="font-semibold">{item.text}</span>
+							</Fragment>
+						))}
+					</div>
+				</div>
+				<Image
+					src="/icon.png"
+					alt="Icon"
+					height={512}
+					width={512}
+					className="w-62 h-62 -m-6.5 -mt-8"
+				/>
+			</div>
+		</>
+	);
 };
 
 export default AboutCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,44 @@
-import Logo from "@/components/Logo";
-import AboutCard from "./cards/about";
-import { Button } from "@/components/ui/button";
 import {
-  IconBrandDiscord,
-  IconBrandGithub,
-  IconBrandX,
+	IconBrandDiscord,
+	IconBrandGithub,
+	IconBrandX,
 } from "@tabler/icons-react";
-import { DicesIcon } from "lucide-react";
+import { RefreshCcwIcon } from "lucide-react";
 import Link from "next/link";
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import AboutCard from "./cards/about";
 
 export default function Home() {
-  return (
-    <div className="max-w-[800px] px-9 mx-auto items-center">
-      <div className="py-9 flex">
-        <Logo />
-      </div>
-      <div className="py-8 px-12 bg-gray-800 border rounded-2xl">
-        <AboutCard />
-        <div className="flex justify-end mt-8">
-          <Button size="lg">
-            <DicesIcon className="size-4" />
-            再表示
-          </Button>
-        </div>
-      </div>
-      <div className="absolute bottom-6 [&_button]:size-10 space-x-2">
-        <Link href="https://twitter.com/IttaFunahashi" target="_blank">
-          <Button>
-            <IconBrandX className="size-5" />
-          </Button>
-        </Link>
-        <Link href="https://github.com/itta611" target="_blank">
-          <Button>
-            <IconBrandGithub className="size-5" />
-          </Button>
-        </Link>
-        <Button>
-          <IconBrandDiscord className="size-5" />
-        </Button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="max-w-[800px] px-9 mx-auto items-center">
+			<div className="py-9 flex">
+				<Logo />
+			</div>
+			<div className="py-8 px-12 bg-gray-800 border rounded-2xl">
+				<AboutCard />
+				<div className="flex justify-end mt-8">
+					<Button size="lg">
+						<RefreshCcwIcon className="size-4" />
+						再表示
+					</Button>
+				</div>
+			</div>
+			<div className="absolute bottom-6 [&_button]:size-10 space-x-2">
+				<Link href="https://twitter.com/IttaFunahashi" target="_blank">
+					<Button>
+						<IconBrandX className="size-5" />
+					</Button>
+				</Link>
+				<Link href="https://github.com/itta611" target="_blank">
+					<Button>
+						<IconBrandGithub className="size-5" />
+					</Button>
+				</Link>
+				<Button>
+					<IconBrandDiscord className="size-5" />
+				</Button>
+			</div>
+		</div>
+	);
 }
