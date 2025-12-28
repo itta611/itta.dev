e933cf281dac20611d2068fdbe733c317c81fdc0
Itta Funahashi <itta@itta.dev>
2025-12-28 16:45:45 +0900

    feat: title大きさ

--- components/card/card-title.tsx
+++ components/card/card-title.tsx
@@ -1,15 +1,15 @@
 import type { FC, ReactNode } from "react";
 
 interface CardTitleProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 const CardTitle: FC<CardTitleProps> = ({ children }) => {
-	return (
-		<div className="font-bold relative text-3xl mb-6 inline-block before:absolute before:-left-2 before:-right-2 before:bottom-0 before:h-4 before:rounded-xs before:bg-gray-700 before:transform">
-			<span className="relative z-10">{children}</span>
-		</div>
-	);
+  return (
+    <div className="font-bold relative text-2xl mb-6 inline-block before:absolute before:-left-2 before:-right-2 before:bottom-0 before:h-4 before:rounded-xs before:bg-gray-700 before:transform">
+      <span className="relative z-10">{children}</span>
+    </div>
+  );
 };
 
 export { CardTitle };
