c07462e282f51466fe2c09c6c0d660766f0f1fc4
Itta Funahashi <itta@itta.dev>
2025-12-30 18:29:43 +0900

    feat: zennのogに対応

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
